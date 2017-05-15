79fbce693768126a64ed5ea866b00c703ce8a32d
Duy Phuong <DuyPhuong@Duys-MacBook-Pro.local>
2017-05-15 18:49:59 +0700

    Initial Commit

---
+++ giaiPT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  giaiPT
+//
+//  Created by Duy Phuong on 5/15/17.
+//  Copyright © 2017 Duy Phuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
